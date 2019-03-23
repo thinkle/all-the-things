8952053a3a4bafd11e28982a4e4a046358be035e
Tom Hinkle <tmhinkle@gmail.com>
2019-03-23 07:44:09 -0400

    ++jump-to-dup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -24,14 +24,33 @@ chrome.runtime.onMessage.addListener(
 		sendResponse({done:false});
 	    }
 	}
-	
+
 	else {
 
 	    //sendResponse('Foo! Bar! Baz!');
 	    lastId += 1;
 	    var actionId = lastId;
-
 	    sendResponse(actionId);
+	    if (request.mode=='jump') {
+		console.log('Jump to dup!');
+		console.log('What to do with request: %s',JSON.stringify(request));
+		chrome.windows.update(request.window.id,{focused:true},function (){});
+		try {
+		    chrome.tabs.update(request.tab.id,{highlighted:true,selected:true});
+		}
+		catch (err) {
+		    console.log('Firefox? Error w/ highlighted');
+		    chrome.tabs.update(request.tab.id,{selected:true});
+		}
+		chrome.tabs.remove(sender.tab.id);
+		completedActions[actionId] = true
+	    }
+	    if (request.mode=='close') {
+		console.log('Close!');
+		console.log('What to do with request: %s',JSON.stringify(request));
+		chrome.tabs.remove(request.tab.id);
+		completedActions[actionId] = true
+	    }
 	    if (request.mode=='getwindows') {
 		function gotWindows (w) {
 		    console.log("Got windows: %s",JSON.stringify(w));
@@ -43,6 +62,9 @@ chrome.runtime.onMessage.addListener(
 		    console.log('getWindows');
 		    getWindows(gotWindows)
 		}
+	    }
+	    if (request.mode=='idtab') {
+		completedActions[actionId] = sender.tab;
 	    }
 	}
     });
